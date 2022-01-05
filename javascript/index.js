f65a2fbea1443fbb6cd23c937b94f9c4d627d823
Rohith Challa <rohithsince2002@gmail.com>
2022-01-05 13:16:52 +0530

    Resolve bugs for form input.

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,16 @@ if (localStorage.getItem('name')){
     nameplacer();
 }
 else{
+    document.querySelector('#name-submit').disabled=true;
     document.getElementById('name-container').classList.add('name-container');
+    document.getElementById('name-input').onkeyup = function(){
+        if(document.querySelector('#name-input').value.length>0){
+            document.querySelector('#name-submit').disabled=false;
+        }
+        else{
+            document.querySelector('#name-submit').disabled=true;
+        }
+    }
     document.querySelector('#name-form').onsubmit = function (){
         var name = document.querySelector('#name-input').value;
         localStorage.setItem('name', name);
