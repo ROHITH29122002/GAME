78ed264909a465b51ff4b698a7b830cb9be4bc6b
Rohith Challa <rohithsince2002@gmail.com>
2022-01-05 16:56:34 +0530

    Define styles for input form.

--- javascript/index.js
+++ javascript/index.js
@@ -8,9 +8,11 @@ else{
     document.getElementById('name-input').onkeyup = function(){
         if(document.querySelector('#name-input').value.length>0){
             document.querySelector('#name-submit').disabled=false;
+            document.querySelector('#name-submit').classList.add('name-submit-green');
         }
         else{
             document.querySelector('#name-submit').disabled=true;
+            document.querySelector('#name-submit').classList.remove('name-submit-green');
         }
     }
     document.querySelector('#name-form').onsubmit = function (){
